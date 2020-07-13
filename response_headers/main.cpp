6c0ae9d39595ec6d2a41aaf641bc549a87501083
David Rappo <david.rappo@gmail.com>
2020-07-13 12:05:03 +1000

    Completing additional exercises for Response Headers tutorial.

--- response_headers/main.cpp
+++ response_headers/main.cpp
@@ -43,6 +43,8 @@ namespace Jade
 
 const std::string large_file{"big.png"};
 const std::string small_file{"picture.png"};
+const std::string count_down{"count_down.html"};
+const unsigned count_down_seconds{60};
 
 } // Jade
 
@@ -55,6 +57,8 @@ int answer_to_connection(void *cls,
     size_t *upload_data_size,
     void **con_cls);
 
+std::string create_count_down_page(unsigned elapsed_time_seconds);
+
 std::string create_error_page();
 
 // Note: decltype(MHD_destroy_response) returns the type of the function MHD_destroy_response. The
@@ -63,6 +67,8 @@ std::unique_ptr<MHD_Response, decltype(MHD_destroy_response)*> create_response(c
     unsigned artificial_delay_seconds);
 
 std::unique_ptr<MHD_Response, decltype(MHD_destroy_response)*> create_error_response();
+
+std::unique_ptr<MHD_Response, decltype(MHD_destroy_response)*> create_count_down_response(unsigned elapsed_time_seconds);
 
 int main(int argc, char **argv)
 {
@@ -73,12 +79,14 @@ int main(int argc, char **argv)
         MHD_USE_INTERNAL_POLLING_THREAD|
         MHD_USE_PEDANTIC_CHECKS|
         MHD_USE_THREAD_PER_CONNECTION;
+    const auto start_time = std::chrono::system_clock::now();
+    const void *start_time_pointer = &start_time;
     auto daemon = MHD_start_daemon(flags,
         port,
         nullptr,
         nullptr,
         &answer_to_connection,
-        nullptr,
+        const_cast<void*>(start_time_pointer),
         MHD_OPTION_END);
     if (nullptr == daemon)
     {
@@ -92,6 +100,23 @@ int main(int argc, char **argv)
     // this of course but it is fine for a simple example program such as this one.
     getchar();
     return 0;
+}
+
+std::string create_count_down_page(unsigned elapsed_time_seconds)
+{
+    std::ostringstream oss;
+    oss << "<html>";
+    oss << "<body>";
+    oss << "<h1>" << Jade::count_down << "</h1>";
+    oss << "<p> Elapsed time in seconds since server started: " << elapsed_time_seconds << "</p>";
+    if (elapsed_time_seconds > Jade::count_down_seconds)
+    {
+        oss << "<p>Count down from " << Jade::count_down_seconds << " seconds complete</p>";
+    }
+    
+    oss << "</body>";
+    oss << "</html>";
+    return oss.str();
 }
 
 std::string create_error_page()
@@ -171,6 +196,40 @@ std::unique_ptr<MHD_Response, decltype(MHD_destroy_response)*> create_error_resp
     return up_response;
 }
 
+std::unique_ptr<MHD_Response, decltype(MHD_destroy_response)*> create_count_down_response(unsigned elapsed_time_seconds)
+{
+    const auto page = create_count_down_page(elapsed_time_seconds);
+    auto response = MHD_create_response_from_buffer(page.length(),
+        const_cast<char*>(page.c_str()),
+        MHD_RESPMEM_MUST_COPY);
+    if (nullptr == response)
+    {
+        throw std::runtime_error{"Failed to create MHD_Response"};
+    }
+    
+    std::unique_ptr<MHD_Response, decltype(MHD_destroy_response)*> up_response{response,
+        MHD_destroy_response};
+    if (elapsed_time_seconds < Jade::count_down_seconds)
+    {
+        using namespace std::string_literals;
+
+        const auto header_string = "Refresh"s;
+        std::ostringstream content_oss;
+        const unsigned delay_seconds{5};
+        // Notify the client that it should request the resource Jade::count_down again in delay_seconds.
+        content_oss << delay_seconds << "; url=/" << Jade::count_down;
+        const auto result = MHD_add_response_header(response, header_string.c_str(), content_oss.str().c_str());
+        if (MHD_NO == result)
+        {
+            std::ostringstream oss;
+            oss << "Failed to add \"" << header_string.c_str() << "\" header to MHD_Response";
+            throw std::runtime_error{oss.str()};
+        }
+    }
+    
+    return up_response;
+}
+
 int answer_to_connection(void *cls,
     struct MHD_Connection *connection,
     const char *url,
@@ -185,7 +244,9 @@ int answer_to_connection(void *cls,
     {
         return MHD_NO;
     }
-
+    
+    assert(nullptr != cls);
+    const auto start_time = *reinterpret_cast<const std::chrono::system_clock::time_point*>(cls);
     auto url_string = std::string(url);
     boost::algorithm::trim(url_string);
     std::unique_ptr<MHD_Response, decltype(MHD_destroy_response)*> up_response{nullptr, MHD_destroy_response};
@@ -198,6 +259,13 @@ int answer_to_connection(void *cls,
     {
         const auto file_name = std::string_view{Jade::small_file.c_str(), Jade::small_file.size()};
         up_response = create_response(file_name, 0);
+    }
+    else if ("/" + Jade::count_down == url_string)
+    {
+        const auto date_time = std::chrono::system_clock::now();
+        const auto difference = date_time - start_time;
+        const auto duration_seconds = std::chrono::duration_cast<std::chrono::seconds>(difference);
+        up_response = create_count_down_response(duration_seconds.count());
     }
 
     return MHD_queue_response(connection, MHD_HTTP_OK, up_response.get());
